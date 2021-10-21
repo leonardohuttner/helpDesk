26e7cf9bfff8a6a38a9323c54af03f587d273aa6
leonardohuttner <leonardohuttner@gmail.com>
2021-10-21 18:08:10 -0300

    first commit

--- src/main/java/models_db/ChamadoDB.java
+++ src/main/java/models_db/ChamadoDB.java
@@ -27,104 +27,158 @@ public class ChamadoDB {
     private Connection conexao = connection.getConexao();
 
     public void save(Chamado chamado) {
-        try {
-            PreparedStatement ps = conexao.prepareStatement("INSERT INTO atendimento (titulo, descricao, id_cliente, data_hora_abertura, id_responsavel, status) values (?,?,?,?,?,?)");
-            ps.setString(1, chamado.getTitulo());
-            ps.setString(2, chamado.getDescricao());
-            ps.setString(3, chamado.getId());
-            ps.setString(4, chamado.getSexo());
-            ps.setString(5, chamado.getEndereco());
-            ps.setString(6, chamado.getCpf());
-            ps.setDate(7, new java.sql.Date(System.currentTimeMillis()));
-            ps.execute();
-            JOptionPane.showMessageDialog(null, "Cadastro efetuado com sucesso!");
-        } catch (SQLException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            try{
+//              Testa se o cliente ja é cliente e pega o id
+                PreparedStatement VC = conexao.prepareStatement("SELECT id "
+                        + "FROM clientes WHERE nome = ?");
+                VC.setString(1,chamado.getNome_cliente());
+                ResultSet VCR = VC.executeQuery();
+                if(VCR.next()){
+                    chamado.setId_cliente(VCR.getInt(1));
+                    JOptionPane.showMessageDialog(null, VCR.getInt("id"));
+            }
+                
+//                Testa e pega o id do responsavel
+                try{
+                    PreparedStatement ps = conexao.prepareStatement("SELECT id FROM responsaveis WHERE nome = ?");
+                    ps.setString(1,chamado.getNome_responsavel());
+                    ResultSet rs = ps.executeQuery();
+                        if(rs != null && rs.next()){
+                            chamado.setId_responsavel(rs.getInt(1));
+                        }
+                    
+//                    se esta tudo correto salva
+                    try {
+                        PreparedStatement cx = conexao.prepareStatement("INSERT INTO atendimento (titulo, descricao, id_cliente, data_hora_abertura, id_responsavel, status) values (?,?,?,?,?,?)");
+                        cx.setString(1, chamado.getTitulo());
+                        cx.setString(2, chamado.getDescricao());
+                        cx.setInt(3, chamado.getId_cliente());
+                        cx.setDate(4, new java.sql.Date(System.currentTimeMillis()));
+                        cx.setInt(5, chamado.getId_responsavel());
+                        cx.setString(6, chamado.getStatus());            
+//                        cx.execute();
+                        JOptionPane.showMessageDialog(null, "Cadastro efetuado com sucesso!");
+                    } catch (SQLException ex) {
+                        Logger.getLogger(Chamado.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }catch(SQLException ex) {
+                    Logger.getLogger(Chamado.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }catch(SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Cliente não encontrado, por favor crie um antes de criar um chamado"); 
+                Logger.getLogger(Chamado.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
     
-    public void update(Cliente cliente) {
+    public void update(Chamado chamado) {
         try {
-            PreparedStatement ps = conexao.prepareStatement("UPDATE clientes SET nome=? , email=?, telefone=?, sexo=?, endereco=?, cpf= ? WHERE id=? ");
-            ps.setString(1, cliente.getNome());
-            ps.setString(2, cliente.getEmail());
-            ps.setString(4, cliente.getSexo());
-            ps.setString(3, cliente.getTelefone());
-            ps.setString(5, cliente.getEndereco());
-            ps.setString(6, cliente.getCpf());
-            ps.setInt(6, cliente.getId());
+            try{
+                PreparedStatement ps = conexao.prepareStatement("SELECT id FROM clientes WHERE nome = ?");
+                ps.setString(1,chamado.getNome_cliente());
+                ResultSet rs = ps.executeQuery();
+                    while(rs.next()){
+                        chamado.setId_cliente(rs.getInt("id"));
+                    }
+            }catch(SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Cliente não encontrado"); 
+                Logger.getLogger(Chamado.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            try{
+                PreparedStatement ps = conexao.prepareStatement("SELECT id FROM responsaveis WHERE nome = ?");
+                ps.setString(1,chamado.getNome_responsavel());
+                ResultSet rs = ps.executeQuery();
+                while(rs.next()){
+                    chamado.setId_responsavel(rs.getInt("id"));
+                }
+            }catch(SQLException ex) {
+                Logger.getLogger(Chamado.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            PreparedStatement ps = conexao.prepareStatement("UPDATE atendimento SET titulo=? , descricao=?, id_cliente=?, data_hora_abertura=?, id_responsavel=?, status= ? WHERE id=? ");
+            ps.setString(1, chamado.getTitulo());
+            ps.setString(2, chamado.getDescricao());
+            ps.setInt(3, chamado.getId_cliente());
+            ps.setDate(4, new java.sql.Date(System.currentTimeMillis()));
+            ps.setInt(5, chamado.getId_responsavel());
+            ps.setString(6, chamado.getStatus()); 
+            ps.setInt(7, chamado.getId());
             ps.execute();
-            JOptionPane.showMessageDialog(null, "Cliente atualizado com sucesso!");
+            JOptionPane.showMessageDialog(null, "Chamado atualizado com sucesso!");
         } catch (SQLException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Chamado.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
         
-    public void saveOrUpdate(Cliente cliente) {
-        if (cliente.getId() > 0) {
-            update(cliente);
+    public void saveOrUpdate(Chamado chamado) {
+        if (chamado.getId() > 0) {
+            update(chamado);
         } else {
-            save(cliente);
+            save(chamado);
         }
     }
     
-    public void delete(Cliente cliente) {
+    public void delete(Chamado chamado) {
         try {
-            PreparedStatement ps = conexao.prepareStatement("DELETE FROM clientes WHERE id=? ");
-            ps.setInt(1, cliente.getId());
+            PreparedStatement ps = conexao.prepareStatement("DELETE FROM atendimento WHERE id=? ");
+            ps.setInt(1, chamado.getId());
             ps.execute();
-            JOptionPane.showMessageDialog(null, "Cliente id "+ cliente.getId()+" deletado com sucesso!");
+            JOptionPane.showMessageDialog(null, "Cliente id "+ chamado.getId()+" deletado com sucesso!");
         } catch (SQLException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Chamado.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public List<Cliente> getAll() {
-         List<Cliente> clientes = new ArrayList<>();
+    public List<Chamado> getAll() {
+         List<Chamado> chamados = new ArrayList<>();
         try {
-            PreparedStatement ps = conexao.prepareStatement("SELECT * FROM clientes");
+            PreparedStatement ps = conexao.prepareStatement("SELECT a.id,a.titulo,"
+                    + "c.nome,a.descricao, a.data_hora_abertura,a.status, r.nome AS nome_responsavel "
+                    + "FROM ifsul.atendimento as a "
+                    + "inner join ifsul.clientes AS c on a.id_cliente = c.id "
+                    + "inner join ifsul.responsaveis as r on a.id_responsavel = r.id;");
             ResultSet rs = ps.executeQuery();
             
             while (rs.next()) {
-                Cliente cliente = new Cliente();
-                cliente.setId(rs.getInt("id"));
-                cliente.setNome(rs.getString("nome"));
-                cliente.setEmail(rs.getString("email"));
-                cliente.setSexo(rs.getString("sexo"));
-                cliente.setTelefone(rs.getString("telefone"));
-                cliente.setCpf(rs.getString("cpf"));
-                cliente.setEndereco(rs.getString("endereco"));
-                cliente.setClienteDesde(rs.getDate("created_at"));
-                clientes.add(cliente);
+                Chamado chamado = new Chamado();
+                chamado.setId(rs.getInt("id"));
+                chamado.setTitulo(rs.getString("titulo"));
+                chamado.setDescricao(rs.getString("descricao"));
+                chamado.setNome_cliente(rs.getString("nome"));
+                chamado.setDescricao(rs.getString("descricao"));
+                chamado.setData_hora_abertura(rs.getDate("data_hora_abertura"));
+                chamado.setNome_responsavel(rs.getString("nome_responsavel"));
+                chamado.setStatus(rs.getString("status"));
+                chamados.add(chamado);
             }
-            return clientes;
+            return chamados;
         } catch (SQLException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Chamado.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     };
     
-    public List<Cliente> getOne(Cliente cliente) {
-         List<Cliente> clientes = new ArrayList<>();
+    public List<Chamado> getOne(Chamado chamado) {
+         List<Chamado> chamados = new ArrayList<>();
         try {
-            PreparedStatement ps = conexao.prepareStatement("SELECT * FROM clientes WHERE nome=?");
-            ps.setString(1, cliente.getNome());
+            PreparedStatement ps = conexao.prepareStatement("SELECT a.id,a.titulo,c.nome,a.descricao,a.data_hora_abertura,r.nome AS nome_responsavel, a.status "
+                    + "FROM ifsul.atendimento AS a , ifsul.clientes AS c,ifsul.responsaveis AS r  "
+                    + "WHERE a.id = c.id AND a.id_responsavel = r.id AND a.id= ?");
+            ps.setInt(1, chamado.getId());
             ResultSet rs = ps.executeQuery();
             
             while (rs.next()) {
-                cliente.setId(rs.getInt("id"));
-                cliente.setNome(rs.getString("nome"));
-                cliente.setEmail(rs.getString("email"));
-                cliente.setSexo(rs.getString("sexo"));
-                cliente.setTelefone(rs.getString("telefone"));
-                cliente.setCpf(rs.getString("cpf"));
-                cliente.setEndereco(rs.getString("endereco"));
-                cliente.setClienteDesde(rs.getDate("created_at"));
-                clientes.add(cliente);
+                chamado.setId(rs.getInt("id"));
+                chamado.setTitulo(rs.getString("titulo"));
+                chamado.setNome_cliente(rs.getString("nome"));
+                chamado.setDescricao(rs.getString("descricao"));
+                chamado.setData_hora_abertura(rs.getDate("data_hora_abertura"));
+                chamado.setNome_responsavel(rs.getString("nome_responsavel"));
+                chamado.setStatus(rs.getString("status"));
+                chamados.add(chamado);
             }
-            return clientes;
+            return chamados;
         } catch (SQLException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Chamado.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
